d4d0f87d707389ac596e54e82a708ff8946bca8b
David Gonzalez <16944187+DavidGonzalez5714@users.noreply.github.com>
2022-12-26 15:21:47 -0500

    added the font awesome icon package (#6)
    * hid next steps and youtube and twitter icons
    
    * added fontawesome icon for testing
    
    * updated build files
    
    * converted svg to fa-icon component
    
    * updated build files

--- DiscipleHandbook/src/app/main-menu/main-menu.component.ts
+++ DiscipleHandbook/src/app/main-menu/main-menu.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faHandsPraying, faBookBible } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.css']
 })
 export class MainMenuComponent implements OnInit {
+  faHeart = faHeart;
+  faHandsPraying = faHandsPraying;
+  faBookBible = faBookBible;
 
   constructor() { }
 
